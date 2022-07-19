35fe1959102823556165972ae7b157ff7dc37643
emretepedev <emretepedev@gmail.com>
2022-07-20 00:09:23 +0300

    refactor(interface): seperate the IOwnable interface

--- contracts/interfaces/19-IAlienCodex.sol
+++ contracts/interfaces/19-IAlienCodex.sol
@@ -2,20 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-interface IOwnable {
-    event OwnershipTransferred(
-        address indexed previousOwner,
-        address indexed newOwner
-    );
-
-    function owner() external view returns (address);
-
-    function isOwner() external view returns (bool);
-
-    function renounceOwnership() external;
-
-    function transferOwnership(address newOwner) external;
-}
+import "./support/IOwnable.sol";
 
 interface IAlienCodex is IOwnable {
     function contact() external view returns (bool);
