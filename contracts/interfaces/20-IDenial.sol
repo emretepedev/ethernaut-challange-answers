a9e5f3ecf565b99ac543cb691daa56cbce1affac
emretepedev <emretepedev@gmail.com>
2022-07-19 03:16:25 +0300

    feat(contract): add new checkers to level 20

---
+++ contracts/interfaces/20-IDenial.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IDenial {
+    function partner() external view returns (address);
+
+    function owner() external view returns (address payable);
+
+    function setWithdrawPartner(address _partner) external;
+
+    function withdraw() external;
+
+    function contractBalance() external view returns (uint256);
+
+    receive() external payable;
+}
