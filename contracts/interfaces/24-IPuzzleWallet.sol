2ad21309e0bed1aa68960160ccc0c61b3db02c85
emretepedev <emretepedev@gmail.com>
2022-07-21 23:28:48 +0300

    feat(contract): add solution to level 24

---
+++ contracts/interfaces/24-IPuzzleWallet.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IPuzzleWallet {
+    function owner() external view returns (address);
+
+    function maxBalance() external view returns (uint256);
+
+    function whitelisted(address) external view returns (bool);
+
+    function balances(address) external view returns (uint256);
+
+    function init(uint256 _maxBalance) external;
+
+    function setMaxBalance(uint256 _maxBalance) external;
+
+    function addToWhitelist(address addr) external;
+
+    function deposit() external payable;
+
+    function execute(
+        address to,
+        uint256 value,
+        bytes calldata data
+    ) external payable;
+
+    function multicall(bytes[] calldata data) external payable;
+}
