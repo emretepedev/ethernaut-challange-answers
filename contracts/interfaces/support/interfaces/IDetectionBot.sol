73cc31e789f3f1926ab4b1ea797fa4306318b532
emretepedev <emretepedev@gmail.com>
2022-07-23 03:43:37 +0300

    feat(contract): add solution to level 26

---
+++ contracts/interfaces/support/interfaces/IDetectionBot.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IDetectionBot {
+    function handleTransaction(address user, bytes calldata msgData) external;
+}
---
+++ contracts/interfaces/support/interfaces/IForta.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface IForta {
+    function setDetectionBot(address detectionBotAddress) external;
+
+    function notify(address user, bytes calldata msgData) external;
+
+    function raiseAlert(address user) external;
+}
